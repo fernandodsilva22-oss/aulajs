c0bb0a4cabf448d57c7ebfdc941807861fdada38
fernandodsilva22-oss <fernando.d.silva22@aluno.senai.br>
2025-09-17 20:55:27 -0300

    Add files via upload

---
+++ Fernando/Aula 17 09/script.js
@@ -0,0 +1,68 @@
+'use strict'; //Ajuda evitar erros no js
+
+const inputNome = document.getElementById('nome');
+//campo de texto do nome
+const inputAno = document.getElementById('ano');
+//campo numérico do ano
+const botaoCalcular = document.getElementById
+('btn-calcular')
+//botao de calcular a idade
+const resultado = document.getElementById('resultado'); //parágrafo onde aparece o resultado
+
+//descobrir ano atual
+const anoAtual = new Date().getFullYear();
+//ler o que o usuúrio digitou
+botaoCalcular.addEventListener('click', () => {
+    const nome = inputNome.value.trim();
+
+    const anoNascStr = inputAno.value.trim();
+
+    const anoNasc = Number(anoNascStr);
+
+    const anoValido = Number.isInteger(anoNasc) && anoNasc >= 1900 && anoNasc <= anoAtual;
+
+    if(!anoValido) {
+        resultado.textContent = 'Por favor, coloque um ano válido';
+        return; //sai da função sem continuar
+}
+const idade = anoAtual - anoNasc;
+
+const nomeDefoult = nome || 'Visitante';
+
+const mensagem = `Olá, ${nomeDefoult}! Você tem ${idade} anos em ${anoAtual}`;
+
+resultado.textContent = mensagem;
+
+inputNome.select()
+
+
+    
+});
+[inputNome,inputAno].forEach(campo => {
+    campo.addEventListener('keydown',
+        (evento) => {
+            if(evento.key === 'Enter') {
+                botaoCalcular.click();
+            }
+        });
+});
+
+botaoCalcular.addEventListener('mouseenter',() => {
+    botaoCalcular.dataset.labelAntiga = botaoCalcular.textContent;
+    botaoCalcular.textContent = 'Clique aqui';
+});
+botaoCalcular.addEventListener('mouseleave',() => {
+    botaoCalcular.textContent = botaoCalcular.dataset.labelAntiga|| 'Calcular idade';
+}
+)
+
+
+
+
+
+
+
+
+
+
+
